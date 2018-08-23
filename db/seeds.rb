b1dc45389051a62a11c657fa7efb1b419bff505e
aude idhammouaubert <aude.idh@gmail.com>
2018-08-23 16:33:03 -0400

    correction of the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,22 @@ Team.destroy_all
 Category.destroy_all
 # Create teams
 
-team_1 = Team.create!(name: “A”)
-team_2 = Team.create!(name: “B”)
+team_1 = Team.create!(name: "A")
+team_2 = Team.create!(name: "B")
 
 # Create users
 
-User.create!(first_name: “1” ,last_name: “Aulas”, email: “e1@free.fr” , password: “azerty”, role: “super_user”)
+User.create!(first_name: "1",last_name: "Aulas", email: "e1@free.fr", password: "azerty", role: "super_user")
+User.create!(first_name: "2",last_name: "Aulas", email: "alex@mailinator.com", password: "azerty", role: "team_leader", team: team_1)
+User.create!(first_name: "3",last_name: "Aulas", email: "e3@free.fr", password: "azerty", role: "team_leader", team: team_2)
+User.create!(first_name: "4",last_name: "Aulas", email: "francis@mailinator.com", password: "azerty", role: "team_member", team: team_1)
+User.create!(first_name: "5",last_name: "Aulas", email: "e5@free.fr", password: "azerty", role: "team_member", team: team_1)
+User.create!(first_name: "6",last_name: "Aulas", email: "e6@free.fr", password: "azerty", role: "team_member", team: team_1)
 
-User.create!(first_name: "2" ,last_name: "Aulas" , email: "alex@mailinator.com" , password: "azerty", role: "team_leader", team: team_1)
-User.create!(first_name: "3" ,last_name: "Aulas" , email: "e3@free.fr" , password: "azerty", role: "team_leader", team: team_2)
-
-User.create!(first_name: "4",last_name: "Aulas" , email: "francis@mailinator.com" , password: "azerty", role: "team_member", team: team_1)
-User.create!(first_name: "5" ,last_name: "Aulas" , email: "e5@free.fr" , password: "azerty", role: "team_member", team: team_1)
-User.create!(first_name: "6",last_name: "Aulas" , email: "e6@free.fr" , password: "azerty", role: "team_member", team: team_1)
-
-User.create!(first_name: “7”,last_name: “Aulas” , email: “e7@free.fr” , password: “azerty”, role: “team_member”, team: team_2)
-User.create!(first_name: “8",last_name: “Aulas” , email: “e8@free.fr” , password: “azerty”, role: “team_member”, team: team_2)
-User.create!(first_name: “9”,last_name: “Aulas” , email: “e9@free.fr” , password: “azerty”, role: “team_member”, team: team_2)
-User.create!(first_name: “10",last_name: “Aulas” , email: “e10@free.fr” , password: “azerty”, role: “team_member”, team: team_2)
+User.create!(first_name: "7",last_name: "Aulas", email: "e7@free.fr", password: "azerty", role: "team_member", team: team_2)
+User.create!(first_name: "8",last_name: "Aulas", email: "e8@free.fr", password: "azerty", role: "team_member", team: team_2)
+User.create!(first_name: "9",last_name: "Aulas", email: "e9@free.fr", password: "azerty", role: "team_member", team: team_2)
+User.create!(first_name: "10",last_name: "Aulas", email: "e10@free.fr", password: "azerty", role: "team_member", team: team_2)
 
 
 # Create surveys
@@ -38,7 +36,7 @@ survey_6 = Survey.create!(team: team_2)
 
 # create! categories
 
-categories = [“Personal growth”, “Well being”, “Collaboration”, “Tools & Processes”, “Enterprise culture”]
+categories = ["Personal growth", "Well being", "Collaboration", "Tools & Processes", "Enterprise culture"]
 
 
 categories.each do |category_name|
@@ -46,7 +44,7 @@ categories.each do |category_name|
  category = Category.create!(name: category_name)
  # filling the surveys with category and questions
  Survey.all.each do |survey|
-   question = Question.create!(category: category, text: “Are you happy?“, survey: survey )
+   question = Question.create!(category: category, text: "Are you happy?", survey: survey )
    # giving 2 responses to the questions
    Response.create!(rating: 5, question: question, user: survey.team.users.all.sample)
    Response.create!(rating: 4, question: question, user: survey.team.users.all.sample)
