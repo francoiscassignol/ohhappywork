d703436a97d6f2d8199641b19886c407045816aa
Alex Grondin <a.grondin@tbs-education.org>
2018-08-30 00:40:07 +0200

    kdo

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -77,7 +77,16 @@ class SurveysController < ApplicationController
     # donner accès à un array de ratings (les 5 derniers)
     # donner accès à un array de labels "date" => qui correspondent aux 5 derniers ratings
 
+    @users = []
+    @responses = []
 
+    Survey.find(params[:id]).team.users.each{|user| @users << user}
+    Survey.find(params[:id]).questions.each {|question| question.responses.each{|response| @responses << response}}  
+
+    @responses_number = @responses.size / 5
+    @users_number = @users.size
+    @pourcentage = (@responses_number.to_f / @users_number.to_f) * 100
+raise
 
   end
 
@@ -100,10 +109,13 @@ class SurveysController < ApplicationController
     Question.create!(category: Category.find_by(name: "Enterprise culture"), text: @enterprise_culture_question_text.sample, survey: @survey )
 
     Team.find(params[:team_id]).users.each do |user|
-      UserMailer.survey(user, @survey).deliver_now
+      UserMailer.survey(user, @survey).deliver_later
     end
 
-  end
+    flash[:notice] = " #{current_user.first_name}, survey launch !"
+
+
+   end
 
    def review_one
      @reviews = Survey.find(params[:survey_id]).reviews
