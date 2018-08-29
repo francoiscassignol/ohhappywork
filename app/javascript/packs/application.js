52520572ee1fb3071f5b06dcf48b53b07ef9481e
Stephanie Trudeau <strudeau1910@gmail.com>
2018-08-29 19:15:15 -0400

    style responses

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,7 @@
 import "bootstrap";
 import runFormAnimation from './form-animation';
 import initChart from './beautiful-chart';
-import needleMove from './needle-animation'
 
 runFormAnimation();
 initChart();
-needleMove();
 
