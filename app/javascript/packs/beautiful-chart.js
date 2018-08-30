34a10fe46aa6edfa39f76dc8dd509f4dd0e54f6a
aude idhammouaubert <aude.idh@gmail.com>
2018-08-29 20:37:48 -0400

    good script added

--- app/javascript/packs/beautiful-chart.js
+++ app/javascript/packs/beautiful-chart.js
@@ -1,29 +1,12 @@
 import Chart from 'chart.js';
-
 function initChart() {
-  const myChartElement = document.getElementById("myChart");
-
-  if (myChartElement === null) { return false; }
-
   const myData = document.querySelector('.ratings_tools');
-
   console.log(myData);
-  const ctx = myChart.getContext('2d');
-
-  Chart.defaults.global.hover.mode = 'nearest';
-
-  Chart.defaults.global.legend.display = false;
-
-  Chart.defaults.global.defaultFontFamily = "'lato'";
-
-  Chart.defaults.global.defaultFontSize = 7;
-
-
+  const ctx = document.getElementById("myChart").getContext('2d');
   Chart.defaults.global.hover.mode = 'nearest';
   Chart.defaults.global.legend.display = false;
   Chart.defaults.global.defaultFontFamily = "'lato'";
   Chart.defaults.global.defaultFontSize = 7;
-
   var myChart = new Chart(ctx, {
       type: 'line',
       data: {
@@ -48,15 +31,13 @@ function initChart() {
                       max:5,
                       stepSize:1,
                       fontFamily: "Lato",
-
                       fontSize: 7
-
                   }
-
               }]
           }
       }
   });
 }
-
 export default initChart;
+
+
